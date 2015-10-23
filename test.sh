4a6c9182ea7ccbd9f1405559dda7d900a7bdcdb3
Xutong <xul041@ucsd.edu>
2015-10-22 17:26:38 -0700

    asdasd

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 for f in $(ls *.m3u)
 do
   grep -qi hq.*mp3$f\
-    && echo -e 'Playlist $f contains a HQ file in map3 format'
+    && echo -e 'I Playlist $f contains a HQ file in map3 format'
 done    
